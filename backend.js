39341cf5be98b711b7465adb53951e6d2ad56ee8
Deepancyberverse <114724024+Deepancyberverse@users.noreply.github.com>
2023-07-20 17:37:33 +0530

    Update backend.js

--- backend.js
+++ backend.js
@@ -1,11 +1,23 @@
-function calculate() {
-  // Get the input values from the user
-  var num1 = parseFloat(document.getElementById("num1").value);
-  var num2 = parseFloat(document.getElementById("num2").value);
+// backend.js
 
-  // Perform the calculation (you can modify this as per your requirements)
-  var sum = num1 + num2;
+function processString() {
+  // Get the input string from the user
+  var inputString = document.getElementById("inputString").value;
 
-  // Display the result to the user
-  document.getElementById("result").textContent = "Result: " + sum;
+  // Send the input string to the backend using fetch
+  fetch('/process', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ string: inputString })
+  })
+  .then(response => response.json())
+  .then(data => {
+    // Display the result received from the backend
+    document.getElementById("result").textContent = "Processed String: " + data.result;
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
 }
